143eea027db0fb619b4f8aa27b69337bc9cc5663
Takeyuki UEDA <ueda@latelierdueda.com>
2019-06-26 10:25:45 +0900

    support error recovery

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get install fswebcam python-pip
-sudo pip install requests pondslider
+sudo pip install requests pondslider error_counter
 #sudo apt-get install curl
 git clone https://github.com/UedaTakeyuki/handlers.git
 ln -s handlers/sensor/uvc/uvc_photo.py
